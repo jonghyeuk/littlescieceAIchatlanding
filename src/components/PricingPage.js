878486b158ddbee8929093793aabddfd561c75b4
jonghyeuk <jonghyeuk@gmail.com>
2025-08-19 16:43:30 +0900

    Update PricingPage.js

--- src/components/PricingPage.js
+++ src/components/PricingPage.js
@@ -91,7 +91,7 @@ const PricingPage = ({ onBackToLanding }) => {
       // 🔥 X-Frame-Options 때문에 iframe 대신 새창 방식 사용
       const form = document.createElement('form');
       form.method = 'POST';
-      form.action = 'https://script.google.com/macros/s/AKfycbyCy2O5iuXn9RlCA5Gj32IyQfSVzXScaKvFtmzcQba_nFAdFNu2d-mPSecOTTb_-Gor/exec';
+      form.action = 'https://script.google.com/macros/s/AKfycbw0Q8wrkX2eES7xTX9e3m8Uw9sUfVa8KhKH315r5MLIFv4034JilKQW9xXLN1pCoYlk/exec';
       form.target = '_blank'; // 🔥 새창에서 열기
 
       // 데이터 추가
